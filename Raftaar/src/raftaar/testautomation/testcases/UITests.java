dff5ba1bc93ae6e16baa833232bf6cae724a2265
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-02-02 14:27:27 +0000

    Updates..

--- Raftaar/src/raftaar/testautomation/testcases/UITests.java
+++ Raftaar/src/raftaar/testautomation/testcases/UITests.java
@@ -1,5 +1,6 @@
 package raftaar.testautomation.testcases;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -11,6 +12,7 @@ import raftaar.testautomation.manager.TestManager;
 public class UITests {
 
 	static TestDriver myDriver = new TestDriver();
+	Logger log = Logger.getLogger("devpinoyLogger");
 
 	@Parameters({ "browser", "testcaseid" })
 	@BeforeClass(alwaysRun = true)
@@ -22,6 +24,8 @@ public class UITests {
 	@Test
 	public void testCase() throws Exception {
 
+		log.debug("This is the message for log file");
+		
 		myDriver.runTestCase(TestManager.MyDataDicitonary.get("testcaseid"));
 	}
 
