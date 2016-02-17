abfd05a44ff2a3b8138208bcaf8bebd355387d4b
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-02-17 11:52:43 +0000

    Updates..

---
+++ Learning_Java/src/others/InheritanceExample2.java
@@ -0,0 +1,5 @@
+package java.examples.set;
+
+public class InheritanceExample2 {
+
+}
---
+++ Learning_Java/src/others/mytestabcd.java
@@ -0,0 +1,19 @@
+package java.examples.set;
+
+import jetr.nsef.api.xmlgenner.XMLGennerSingleton;
+import jetr.nsef.api.xmlgenner.XmlGenner;
+
+public class mytestabcd {
+
+	public static void main(String args[]) {
+
+		try {
+			XmlGenner x = XMLGennerSingleton.getXmlGenner();
+			x.setCustomRows(1000);
+			x.setSubRootElementName("TestCases");
+			x.generateXML_Item("c:/gk/employee.xls", "c:/gk/out.xml");
+		} catch (Exception ex) {
+			// handler
+		}
+	}
+}
