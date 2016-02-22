1034110a19912b83402d125c05eebf50a03031b9
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-02-22 14:52:38 +0000

    Updates..

---
+++ Learning_Java/src/others/ConvertStringToList.java
@@ -0,0 +1,20 @@
+package others;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertStringToList {
+
+	public static void main(String[] args) {
+		
+		String pattern = "Argument 1 | Argument 2 | Argument 3";
+		String strArr[] = pattern.split("\\|");
+		List<String> models = Arrays.asList(strArr);
+	      // Or like this...
+        for(int i = 0; i < models.size(); i++) {
+            System.out.println(models.get(i).toString().trim());
+        }
+
+	}
+
+}
