b2b70bd396e71726fb98f5bb19e941db2cb53a69
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-03-04 12:26:44 +0000

    Updates..

--- Raftaar/src/raftaar/testautomation/testcases/UITests.java
+++ Raftaar/src/raftaar/testautomation/testcases/UITests.java
@@ -25,8 +25,6 @@ public class UITests {
 		TestManager.MyDataDicitonary.put("browser", browser);
 		TestManager.MyDataDicitonary.put("testcaseid", testcaseid);
 		TestManager.MyDataDicitonary.put("DBConnection", DBConnection);
-		// TestManager.MyDataDicitonary.put("RemoteURL",
-		// "http://192.168.49.17:4444/wd/hub");
 		TestManager.MyDataDicitonary.put("RemoteURL", "local");
 
 	}
