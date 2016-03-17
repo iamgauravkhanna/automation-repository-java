e58212fefb747caa99483026f725a68da4b67528
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-03-17 14:44:22 +0000

    Updates..

--- WebDriver_Scripts/src/webdriverScripts/PopupsHandling1.java
+++ WebDriver_Scripts/src/webdriverScripts/PopupsHandling1.java
@@ -10,13 +10,13 @@ public class PopupsHandling1 {
 	WebDriver driver = new FirefoxDriver();
 
 	@Test
-	public void ExampleForAlert() throws InterruptedException {
+	public void ExampleForAlert() throws InterruptedException {		
+		driver.get("file:///C:/Code/Selenium/Raftaar/html/alerts.html");
 		driver.manage().window().maximize();
-		driver.get("file:///C:/Selenium/Raftaar/html/alerts.html");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//button[@onclick='alertFunction()']")).click();
-		Alert alert = driver.switchTo().alert();
-		System.out.println(alert.getText());
-		alert.accept();
+/*		Alert alert = driver.switchTo().alert();
+		System.out.println("Alert Text :  "+alert.getText());
+		alert.accept();*/
 	}
 }
