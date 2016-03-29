d0b08c619296bf61ddc9a4fc6f90251670c499c8
grvkhanna <grvkhanna@d97a6823-2c30-41b8-9d82-b8eedc8222d2>
2016-03-29 12:33:43 +0000

    Updates

--- Raftaar/src/raftaar/testautomation/framework/JarExample.java
+++ Raftaar/src/raftaar/testautomation/framework/JarExample.java
@@ -7,21 +7,23 @@ public class JarExample {
 
 	public static void main(String[] args) throws Exception {
 
-		String a,b,c,d ;
-		
-/*		String a = args[0];
+		String c, d;
+
+		String a = args[0];
 		String b = args[1];
-		String c = args[2];
-		String d = args[3];*/
-		a = "Firefox";
-		b = "Test.Case.035";
+		/*
+		 * String c = args[2]; String d = args[3];
+		 */
+		/*
+		 * a = "Firefox"; b = "Test.Case.035";
+		 */
 		c = "jdbc:oracle:thin:PHIX_QA08_001/pr1vate@phixdevproduct.cq66g54vnu5m.us-east-1.rds.amazonaws.com:1521:phix";
 		d = "local";
 		TestManager.MyDataDicitonary.put("browser", a);
 		TestManager.MyDataDicitonary.put("testcaseid", b);
 		TestManager.MyDataDicitonary.put("DBConnection", c);
 		TestManager.MyDataDicitonary.put("RemoteURL", d);
-		
+
 		System.out.println("Starting Test Case");
 		UITests testObject = new UITests();
 		testObject.testCase();
