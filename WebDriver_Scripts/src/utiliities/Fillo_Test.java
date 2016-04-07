6d907cd87d8bbd5140650ca67d10e7ea6f1e6080
Gaurav Khanna <iamgauravkhanna@gmail.com>
2016-04-07 16:02:44 +0530

    Changing file path

--- WebDriver_Scripts/src/utiliities/Fillo_Test.java
+++ WebDriver_Scripts/src/utiliities/Fillo_Test.java
@@ -1,21 +1,19 @@
 package utiliities;
+
 import Exception.FilloException;
 import Fillo.Connection;
 import Fillo.Fillo;
 import Fillo.Recordset;
 
 public class Fillo_Test {
-	
-	// Reference :  
 
 	public static void main(String[] args) throws FilloException {
 
 		Fillo fillo = new Fillo();
 
-		Connection connection = fillo
-				.getConnection("C:\\GK\\Selenium\\iAutomation\\testCases\\testcase.xlsx");
+		Connection connection = fillo.getConnection("C:\\Selenium\\testCases\\testcase.xlsx");
 
-		String strQuery = "Select * from Employer where description IS NULL";
+		String strQuery = "Select * from user_data where description IS NULL";
 
 		Recordset recordset = connection.executeQuery(strQuery);
 
