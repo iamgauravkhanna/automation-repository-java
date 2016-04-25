7a00e1c5d63aac366e0ae11bbd382df35410c351
Gaurav Khanna <iamgauravkhanna@gmail.com>
2016-04-25 19:30:09 +0530

    Adding New Java Examples

---
+++ Learning_Java/src/javaExamples/strings/StringTestExercise.java
@@ -0,0 +1,13 @@
+package javaExamples.strings;
+
+public class StringTestExercise {
+
+	public static void main(String[] args) {
+
+		char[] webSiteName = { 'T', 'O', 'O', 'L', 'S', 'Q', 'A' };
+		String webSite = new String(webSiteName);
+		System.out.println(webSite);
+
+	}
+
+}
