162f67fe39f5d1140f00df2f3916a8c4f9222527
Gaurav Khanna <iamgauravkhanna@gmail.com>
2016-04-26 16:17:06 +0530

    Adding New Scripts

---
+++ WebDriver_Scripts/src/webdriverScripts/browsers/InternetExplorerBrowser.java
@@ -0,0 +1,50 @@
+/*********************************
+ 
+Title  : Internet Explorer Browser
+Author : Gaurav Khanna
+
+ *********************************/
+
+package webdriverScripts.browsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class InternetExplorerBrowser {
+
+	// Declaring variable 'webDriver' of WebDriver Type
+	WebDriver webDriver;
+
+	// Declaring baseURL variable of String Type
+	String baseUrl;
+
+	@Test
+	public void testusingInternetExplorerBrowser() {
+
+		// Setting IE Driver Path
+		System.setProperty("webdriver.ie.driver",
+				"C:\\gk\\code\\selenium\\lib\\IEDriverServer.exe");
+
+		// Initializing IE Browser
+		webDriver = new InternetExplorerDriver();
+
+		// Assigning URL to variable baseUrl
+		baseUrl = "http://not-just-a-tester.blogspot.in";
+
+		// Open the link
+		webDriver.get(baseUrl);
+
+		// Maximize the window
+		webDriver.manage().window().maximize();
+
+		// Click on Selenium link
+		webDriver.findElement(By.linkText("Selenium")).click();
+
+		// This will close the browser
+		webDriver.quit();
+
+	}
+
+}
