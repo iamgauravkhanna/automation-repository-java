36e21c78b958c7b33a241fdad3bcb50838b750e2
Gaurav Khanna <iamgauravkhanna@gmail.com>
2016-05-11 18:24:21 +0530

    Updates...

--- Raftaar_Core/src/raftaar/testautomation/framework/Framework.java
+++ Raftaar_Core/src/raftaar/testautomation/framework/Framework.java
@@ -1,5 +1,9 @@
 package raftaar.testautomation.framework;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import raftaar.testautomation.manager.TestManager;
 import raftaar.testautomation.testcases.UITests;
 
@@ -7,12 +11,25 @@ public class Framework {
 
 	public static void main(String[] args) throws Exception {
 
+		Properties prop = new Properties();
+		InputStream input = null;
 		String a, b, c, d;
 
-		a = "Firefox";
-		b = "Test.Case.001";
-		c = "jdbc:oracle:thin:username/password@localhost:1521:xe";
-		d = "local";
+		input = new FileInputStream(System.getProperty("user.dir") + "\\"+ "config.properties");
+
+		// load a properties file
+		prop.load(input);
+
+		// get the property value and print it out
+		a = prop.getProperty("browser");
+		b = prop.getProperty("testcaseid");
+		c = prop.getProperty("database");
+		d = prop.getProperty("remortUrl");
+
+		/*
+		 * a = "Firefox"; b = "Test.Case.001"; c =
+		 * "jdbc:oracle:thin:username/password@localhost:1521:xe"; d = "local";
+		 */
 
 		TestManager.MyDataDicitonary.put("browser", a);
 		TestManager.MyDataDicitonary.put("testcaseid", b);
