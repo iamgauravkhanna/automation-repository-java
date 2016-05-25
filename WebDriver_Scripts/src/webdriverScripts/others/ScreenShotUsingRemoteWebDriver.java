5d98d2128fbf37ed3d5b505b259c3c9a9da11378
Gaurav Khanna <iamgauravkhanna@gmail.com>
2016-05-25 17:08:07 +0530

    Adding New Examples

--- WebDriver_Scripts/src/webdriverScripts/others/ScreenShotUsingRemoteWebDriver.java
+++ WebDriver_Scripts/src/webdriverScripts/others/ScreenShotUsingRemoteWebDriver.java
@@ -14,22 +14,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class ScreenShotUsingRemoteWebDriver {
-	public static void main(String... args){
+	public static void main(String... args) {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setBrowserName("firefox");
 		WebDriver remoteWD = null;
 		try {
-			remoteWD = 
-					new RemoteWebDriver(new URL("http://<remote-webdriver-ip>:4444/wd/hub"),capabilities);
+			remoteWD = new RemoteWebDriver(new URL("http://192.168.49.32:4444/wd/hub"), capabilities);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		remoteWD.get("http://www.google.com");
 		WebElement element = remoteWD.findElement(By.name("q"));
 		element.sendKeys("Packt Publishing");
-		
+
 		remoteWD = new Augmenter().augment(remoteWD);
-		File scrFile = ((TakesScreenshot)remoteWD).getScreenshotAs(OutputType.FILE);
-        System.out.println(scrFile.getAbsolutePath());
+		File scrFile = ((TakesScreenshot) remoteWD).getScreenshotAs(OutputType.FILE);
+		System.out.println(scrFile.getAbsolutePath());
 	}
 }
