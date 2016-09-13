5d7a857d91835e62b861c2db6a4d0fd3ed7e55a0
Grv <iamgauravkhanna@gmail.com>
2016-09-13 17:11:55 +0530

    Updates

---
+++ learning-junit/src/junitExamples/WhatsMyJunitVersion.java
@@ -0,0 +1,11 @@
+package junitExamples;
+
+import junit.runner.Version;
+
+public class WhatsMyJunitVersion {
+
+	public static void main(String args[]) {
+
+		System.out.println("JUnit version is: " + Version.id());
+	}
+}
