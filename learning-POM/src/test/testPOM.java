7f93ee6978a4a629318924ab2afd23d4ebb070e5
Grv <iamgauravkhanna@gmail.com>
2016-09-27 17:16:46 +0530

    Updates

---
+++ learning-POM/src/test/testPOM.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+
+public class testPOM {
+
+	WebDriver myDriver;
+
+	HomePage objHomePage;
+
+	@BeforeTest
+	public void setup() {
+
+		myDriver = new FirefoxDriver();
+
+		myDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		myDriver.get("http://book.theautomatedtester.co.uk/");
+
+	}
+
+	@Test
+	public void FirstTestCase() {
+
+		// Create Home Page Object
+		objHomePage = new HomePage(myDriver);
+
+		// Verify Home Page Text
+		objHomePage.verifyHomePageText();
+
+		// Click on First Link
+		objHomePage.clickOnLinkChapter1();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+
+		myDriver.quit();
+	}
+}
