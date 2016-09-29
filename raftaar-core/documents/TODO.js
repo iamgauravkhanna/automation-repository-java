b3b878f03e8d5974b2c904a8819d7b3d02ffa173
Grv <iamgauravkhanna@gmail.com>
2016-09-29 17:53:46 +0530

    Updates

--- raftaar-core/documents/TODO.js
+++ raftaar-core/documents/TODO.js
@@ -1,10 +1,16 @@
-Way to Run JavaScript on Browser
-Way to Run JavaScript without Browser
-Add capability to read all sheets in loop and store them in map except KeywordList, FlagsList and Template sheets
-Way to assert link status
-Download file from Internet
-Click and Download feature within browser
-updateExcelCell
-getPageUrl
-saveFile
-uploadFile
+Ability to read all sheets in loop and store TestCases and TestData in map except KeywordList, FlagsList and Template sheets
+Ability to read parameters passed as arguments for called Test Case ID
+Implement Flag optional
+Implement Flag nowait
+Implement Flag nolog
+Implement Flag knownDefect=DEFECT-001
+Implement Flag normalize
+Implement Keyword updateExcelCell
+Implement Keyword getPageUrl
+Implement Keyword saveFile
+Implement Keyword uploadFile
+Implement Keyword runJavaScript
+Implement Keyword runJavaScriptOnBrowser
+Implement Keyword assertLinkStatus
+Implement Keyword downloadFile
+Implement Keyword clickAndDownload
--- raftaar-core/src/raftaar/testautomation/testcases/RunTestCase.java
+++ raftaar-core/src/raftaar/testautomation/testcases/RunTestCase.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 
 import raftaar.testautomation.framework.Framework;
 
-
 /**
  * This is the beginning point of Framework
  * 
@@ -13,28 +12,10 @@ public class RunTestCase {
 
 	Framework F1 = new Framework();
 
-/*	@Test
-	public void firstTC() throws Exception {
-
-		F1.runTestCase("Bodgeit.Workflow");
-	}
-
-	@Test
-	public void secondTC() throws Exception {
-
-		F1.runTestCase("Test.Case.001");
-	}
-
-	@Test
-	public void thirdTC() throws Exception {
-
-		F1.runTestCase("Test.Case.002");
-	}*/
-
 	@Test
 	public void fourthTC() throws Exception {
 
-		F1.runTestCase("Broker.Employer.53602.013");
+		F1.runTestCase("Regression.Workflow.001");
 	}
 
 }
