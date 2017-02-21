dea40a22307d6b9d05ec598bebc63139a03ebf2e
iamgauravkhanna@gmail.com <iamgauravkhanna@gmail.com>
2017-02-21 17:31:41 +0530

    Updates

--- learning-java/src/javaExamples/strings/StringTestExercise.java
+++ learning-java/src/javaExamples/strings/StringTestExercise.java
@@ -4,7 +4,7 @@ public class StringTestExercise {
 
 	public static void main(String[] args) {
 
-		char[] webSiteName = { 'T', 'O', 'O', 'L', 'S', 'Q', 'A' };
+		char[] webSiteName = { 'G', 'A', 'U', 'R', 'A', 'V' };
 		String webSite = new String(webSiteName);
 		System.out.println(webSite);
 
--- learning-java/src/javaExamples/strings/StringTestExercise1.java
+++ learning-java/src/javaExamples/strings/StringTestExercise1.java
@@ -3,12 +3,15 @@ package javaExamples.strings;
 public class StringTestExercise1 {
 
 	public static void main(String[] args) {
+
 		// Declaring the String and Int Variable
 		String sPopularTopic = "Selenium Automation Framework";
+
 		int ilength = sPopularTopic.length();
 
 		// Print the value of String variable & int Variable
-		System.out.println("Popular Topic of ToolsQA: " + sPopularTopic);
+		System.out.println("Popular Topic of QA: " + sPopularTopic);
+
 		System.out.println("Length of the Popular Topic: " + ilength);
 
 	}
