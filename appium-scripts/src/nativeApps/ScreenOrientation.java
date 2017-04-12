115ccaa9961d267e47a127ed7a5c03cc6f952401
iamgauravkhanna@gmail.com <iamgauravkhanna@gmail.com>
2017-04-12 19:38:49 +0530

    Adding New Scripts

--- appium-scripts/src/nativeApps/ScreenOrientation.java
+++ appium-scripts/src/nativeApps/ScreenOrientation.java
@@ -37,22 +37,30 @@ public class ScreenOrientation {
 
 	@Test
 	public void performOrientation() throws InterruptedException {
+
 		// Get and print current screen orientation.
 		System.out.println("*--*--*-- Current screen orientation Is : " + driver.getOrientation());
+
 		System.out.println("*--*--*-- Changing screen Orientation to LANDSCAPE.");
 		// Changing screen Orientation to LANDSCAPE
 		driver.rotate(org.openqa.selenium.ScreenOrientation.LANDSCAPE);
+
 		// Get and print screen orientation after changing It.
 		System.out.println("*--*--*-- Now screen orientation Is : " + driver.getOrientation());
+
 		Thread.sleep(5000);
 		// Scroll till element which contains "Views" text If It Is not visible
 		// on screen.
 		driver.findElement(By.name("Views")).click();
+
 		System.out.println("*--*--*-- Changing screen Orientation to PORTRAIT.");
 		// Changing screen Orientation to PORTRAIT.
+
 		driver.rotate(org.openqa.selenium.ScreenOrientation.PORTRAIT);
+
 		// Get and print screen orientation after changing It.
 		System.out.println("*--*--*-- Now screen orientation Is : " + driver.getOrientation());
+
 		Thread.sleep(5000);
 	}
 
