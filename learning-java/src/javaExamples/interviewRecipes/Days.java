9c3c5d496562fc8a0f45f39f3c0b2f01483e72c9
Gaurav Khanna (GRV) <iamgauravkhanna@gmail.com>
2018-09-04 15:34:53 +0530

    New Fixes

---
+++ learning-java/src/javaExamples/interviewRecipes/Days.java
@@ -0,0 +1,17 @@
+package javaExamples.interviewRecipes;
+
+// Program to convert given number of days into months and days
+
+class Days {
+
+	public static void main(String args[]) {
+
+		int n, m, d;
+		n = 360;
+
+		m = n / 30;
+		d = n % 30;
+
+		System.out.println(n + " days" + " = " + m + " months " + "and " + d + " days");
+	}
+}
